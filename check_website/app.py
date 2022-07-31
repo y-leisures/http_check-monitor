9385e9b13d06a0f7d25be9f4d1a413ea80dcd5e5
Yukihiro Okada <yuokada@users.noreply.github.com>
2022-08-01 02:18:52 +0900

    Update template.yaml

--- check_website/app.py
+++ check_website/app.py
@@ -10,6 +10,7 @@ import twitter
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
+# from boto3.dynamodb.conditions import Key
 from common import get_secret
 
 
