31d1cf5732a00e3710931cb3ee7c94317cf147b0
Yukihiro Okada <yuokada@users.noreply.github.com>
2023-01-22 02:11:25 +0900

    Remove hard-coded environment variable

--- check_website/notify_to_slack.py
+++ check_website/notify_to_slack.py
@@ -3,11 +3,9 @@ import os
 import requests
 from requests import Response
 
-DEFAULT_WEBHOOK_URL = 'https://hooks.slack.com/services/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'
-
 
 def notify_to_slack(payload: dict) -> Response:
-    webhook_url = os.getenv('SLACK_WEBHOOK_URL', DEFAULT_WEBHOOK_URL)
+    webhook_url = os.getenv('SLACK_WEBHOOK_URL')
     response: Response = requests.post(
         webhook_url,
         json=payload,
